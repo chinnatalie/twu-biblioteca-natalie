ffbe37229bc39b713fa16d93c9c2964d0745da32
Natalie Chin <chin_natalie@yahoo.com.sg>
2019-02-07 16:45:43 +0800

    [natalie] [#1.2] remove unused list of books string

--- src/com/twu/biblioteca/BibliotecaUI.java
+++ src/com/twu/biblioteca/BibliotecaUI.java
@@ -7,11 +7,6 @@ class BibliotecaUI {
     private BibliotecaShelf shelf;
 
     private String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
-    private String listOfAllBooksWithAuthorAndPublishedYear = "Rainbirds | Clarissa Goenawan | 2018\n" +
-            "Bury What We Cannot Take | Kirsten Chen | 2018\n" +
-            "An Ocean of Minutes | Thea Lim | 2018\n" +
-            "The Descent of Monsters (The Tensorate Series) | JY Yang | 2018\n" +
-            "Ponti | Sharlene Teo | 2018\n";
     private String mainMenu = "------- Main menu -------\n" +
             "1) List of books\n" +
             "2) Exit";

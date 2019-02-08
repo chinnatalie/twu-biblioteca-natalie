dcdffb14b7906f493bd524a8ddcf9bcf8b9bfb22
Natalie Chin <chin_natalie@yahoo.com.sg>
2019-02-08 11:45:44 +0800

    [natalie] [#1.12] refactor out success checkout message

--- src/com/twu/biblioteca/BibliotecaUI.java
+++ src/com/twu/biblioteca/BibliotecaUI.java
@@ -18,6 +18,7 @@ class BibliotecaUI {
     private String returnBookQuestion = "Which book do you want to return?";
     private String successReturnMessage = "Thank you for returning the book";
     private String failureReturnMessage = "That is not a valid book to return.";
+    private String successCheckoutMessage = "Thank you! Enjoy the book";
 
 
     BibliotecaUI() {
@@ -79,7 +80,7 @@ class BibliotecaUI {
     void checkoutBook(String bookName) {
         switch (shelf.checkoutBook(bookName)) {
             case SUCCESS:
-                System.out.println("Thank you! Enjoy the book");
+                System.out.println(successCheckoutMessage);
                 break;
             case FAILURE:
                 System.out.println("Sorry, that book is not available");

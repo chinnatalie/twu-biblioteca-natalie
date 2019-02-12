2bd37dd9044653b7488f6f2f15dfafefdbdd120d
Natalie Chin <chin_natalie@yahoo.com.sg>
2019-02-12 15:18:00 +0800

    [natalie] [#2.3] add getDetails method in Account

--- src/com/twu/biblioteca/BibliotecaAccount.java
+++ src/com/twu/biblioteca/BibliotecaAccount.java
@@ -3,10 +3,21 @@ package com.twu.biblioteca;
 public class BibliotecaAccount {
     private String libraryNumber;
     private String password;
+    private String name;
+    private String email;
+    private String phoneNumber;
     
     BibliotecaAccount(String libraryNumber, String password) {
         this.libraryNumber = libraryNumber;
         this.password = password;
+    }
+
+    BibliotecaAccount(String libraryNumber, String password, String name, String email, String phoneNumber) {
+        this.libraryNumber = libraryNumber;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
     public boolean authenticate(String password) {
@@ -21,5 +32,14 @@ public class BibliotecaAccount {
             return true;
         else
             return false;
+    }
+
+    public String getDetails() {
+        String details = "";
+        details += "Library number: " + libraryNumber + "\n";
+        details += "Name: " + name + "\n";
+        details += "Email: " + email + "\n";
+        details += "Phone number: " + phoneNumber + "\n";
+        return details;
     }
 }
--- test/com/twu/biblioteca/BibliotecaAccountTest.java
+++ test/com/twu/biblioteca/BibliotecaAccountTest.java
@@ -10,18 +10,33 @@ public class BibliotecaAccountTest {
 
     private BibliotecaAccount account;
 
+    private final String libraryNumber = "123-4567";
+    private final String password = "password123";
+    private final String name = "Tan Ting Gi";
+    private final String email = "tinggi.tan@thoughtworks.com";
+    private final String phoneNumber = "+65 9876 5432";
+    private final String accountDetails = "Library number: 123-4567\n" +
+            "Name: Tan Ting Gi\n" +
+            "Email: tinggi.tan@thoughtworks.com\n" +
+            "Phone number: +65 9876 5432\n";
+
     @Before
     public void initializeAccount() {
-        account = new BibliotecaAccount("123-4567", "password123");
+        account = new BibliotecaAccount(libraryNumber, password, name, email, phoneNumber);
     }
 
     @Test
     public void shouldReturnTrueIfCorrectPassword() {
-        assertThat(account.authenticate("password123"), is(true));
+        assertThat(account.authenticate(password), is(true));
     }
 
     @Test
     public void shouldReturnFalseIfWrongPassword() {
         assertThat(account.authenticate("pazzwork321"), is(false));
+    }
+
+    @Test
+    public void shouldReturnAccountDetails() {
+        assertThat(account.getDetails(), is(accountDetails));
     }
 }
